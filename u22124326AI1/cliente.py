be2425b78518668891ef09584e6d4a9901c4437e
Ana Esteban <ana.esteban@balanceit.org>
2022-11-17 20:48:41 +0100

    Pruebas nickname

--- u22124326AI1/cliente.py
+++ u22124326AI1/cliente.py
@@ -6,13 +6,14 @@ import os
 
 class Cliente():
 
-	def __init__(self, host=input("Intoduzca la IP del servidor ?  "), port=int(input("Intoduzca el PUERTO del servidor ?  "))):
+	def __init__(self, host = input("Intoduzca la IP del servidor ?  "), port = int(input("Intoduzca el PUERTO del servidor ?  "))):
 		self.s = socket.socket()
 		self.s.connect((host, int(port)))
 		print('\n\tProceso con PID = ',os.getpid(), '\n\tHilo PRINCIPAL con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(),'\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
 		threading.Thread(target=self.recibir, daemon=True).start()
 
-		nick = input("Ingresa tu nickname:")
+		nick = input('Ingresa tu nickname:')
+		print('Nickname')
 		while True:
 			msg = input('\nEscriba texto ?   ** Enviar = ENTER   ** Salir Chat = 1 \n')
 			if msg == '1':
@@ -21,7 +22,7 @@ class Cliente():
 				sys.exit()
 			else:
 				with open("22124326.txt", "a") as f:
-					msg = input("Escriba el mensaje:")
+					msg = input('Escriba el mensaje:')
 					f.write("\n" + nick + " - " + msg)
 					self.enviar(msg)
 				
