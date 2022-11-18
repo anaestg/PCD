ffc8c72d111603488711bd2696e4ee37903ab049
Ana Esteban <ana.esteban@balanceit.org>
2022-11-18 11:53:58 +0100

    Cambios a cliente

--- u22124326AI1/cliente.py
+++ u22124326AI1/cliente.py
@@ -6,29 +6,31 @@ import os
 
 class Cliente():
 
-	def __init__(self, host = input("Intoduzca la IP del servidor ?  "), port = int(input("Intoduzca el PUERTO del servidor ?  "))):
+	def __init__(self, host = input("Intoduzca la IP del servidor: "), port = int(input("Intoduzca el PUERTO del servidor: "))):
+		nick = input('Introduzca su nickname: ')
 		self.s = socket.socket()
 		self.s.connect((host, int(port)))
 		print('\n\tProceso con PID = ',os.getpid(), '\n\tHilo PRINCIPAL con ID =',threading.currentThread().getName(), '\n\tHilo en modo DAEMON = ', threading.currentThread().isDaemon(),'\n\tTotal Hilos activos en este punto del programa =', threading.active_count())
-		threading.Thread(target=self.recibir, daemon=True).start()
+		threading.Thread(target = self.recibir, daemon = True).start()
 
-		nick = input('Ingresa tu nickname:')
-		print('Nickname')
+		self.enviar(nick)
+		
 		while True:
-			msg = input('\nEscriba texto ?   ** Enviar = ENTER   ** Salir Chat = 1 \n')
+			msg = input('\n ¿Quiere seguir en el chat?   ** Si = ENTER   ** Salir Chat = 1 \n')
 			if msg == '1':
 				print(" **** Me piro vampiro; cierro socket y mato al CLIENTE con PID = ", os.getpid())
 				self.s.close()
 				sys.exit()
 			else:
 				with open("22124326.txt", "a") as f:
-					msg = input('Escriba el mensaje:')
+					msg = input('Escriba el mensaje: ')
 					f.write("\n" + nick + " - " + msg)
+					msg = nick + ': ' + msg
 					self.enviar(msg)
 				
 
 	def recibir(self):
-		print('\nHilo RECIBIR con ID =',threading.currentThread().getName(), '\n\tPertenece al PROCESO con PID', os.getpid(), "\n\tHilos activos TOTALES ", threading.active_count())
+		print('\n Hilo RECIBIR con ID =',threading.currentThread().getName(), '\n\t Pertenece al PROCESO con PID', os.getpid(), "\n\t Hilos activos TOTALES ", threading.active_count())
 		while True:
 			try:
 				data = self.s.recv(32)
